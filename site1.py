23db581950d11d05b0c65776106c0659778cafe9
danielhslmello <150366606+danielhslmello@users.noreply.github.com>
2023-12-21 16:18:39 -0300

    Update site1.py

--- site1.py
+++ site1.py
@@ -1,3 +1,29 @@
 import streamlit as st
 
-st.write('Hello world!')
+#Conras Imports
+import qrcode
+#import os
+from PIL import Image, ImageDraw, ImageFont
+
+#Formulario
+st.markdown("<h1 style= 'text-align: center;'> Gerador de <br>Cartão de visita</h1>", unsafe_allow_html=True)
+with st.form("Form 1"):
+  #Campos
+  col1,col2=st.columns([0.6, 0.4])
+  f_name = col1.text_input("Nome")
+  cargo = col1.text_input("Cargo")
+  email = col1.text_input("e-mail")
+  unidade = col1.text_input("Unidade")
+  endereco = col1.text_input("Endereco")
+  cidade = col1.text_input("Cidade")
+  tel_agenda = col1.text_input("Tel_agenda")
+  tel_sec = col1.text_input("Tel_sec")
+  tel_pessoa = col1.text_input("Tel_pessoal")
+
+  #Botão
+  s_state=col1.form_submit_button("Submit", on_click=None )
+  if s_state: # Campos Obrigatorios
+    if f_name == "" or unidade == "":
+      st.warning("Campos Obrigatorios")
+#End Formulario
+
